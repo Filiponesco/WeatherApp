3d7c1b8fee36d75289f1bdb97909cfe6c4db43d5
MyisCARRY <tataauto@interia.pl>
2020-04-01 16:27:56 +0200

    Added swipe to delete. Swipe to refresh. Fixed error messages

--- weather_broadcast/lib/pages/home_page.dart
+++ weather_broadcast/lib/pages/home_page.dart
@@ -1,16 +1,14 @@
 import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:weather_broadcast/models/forecast_entity.dart';
-import 'package:connectivity/connectivity.dart';
+import 'package:weather_broadcast/additional_functions.dart';
 import 'package:weather_broadcast/widgets/forecast_lookup.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 import '../repository.dart';
 import 'forecast_page.dart';
-
-import 'package:geolocator/geolocator.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -19,97 +17,128 @@ class HomePage extends StatefulWidget {
     return HomePageState();
   }
 }
+
 class HomePageState extends State<HomePage> {
   final repo = Repository.getInstance();
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
+  Future<List<Future<ForecastEntity>>> _forecasts;
 
-
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    Future<List<Future<ForecastEntity>>> _forecasts =
-    repo.allSavedCities().then((result) {
+    _forecasts = repo.allSavedCities().then((result) {
       return List.generate(result.length, (i) {
         return repo.getBroadcastForCity(result[i]);
       });
     });
 
-//    _getCurrentLocation();
+    Widget _futureLookup(Future<ForecastEntity> forecast,
+        {bool favorite = false, bool dismissible = false}) {
+      return FutureBuilder(
+        future: forecast,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData) {
+              (snapshot.data as ForecastEntity).favorite = favorite;
+              return dismissible
+                  ? Dismissible(
+                      child: ForecastLookup(forecast: snapshot.data),
+                      key: Key((snapshot.data as ForecastEntity).name),
+                      onDismissed: (direction) {
+                        setState(() {
+                          repo.deleteCity(
+                              (snapshot.data as ForecastEntity).name);
+                        });
+                      },
+                    )
+                  : ForecastLookup(forecast: snapshot.data);
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text("Problem with connection to API"),
+              );
+            }
+          }
+          return Center(child: CircularProgressIndicator());
+        },
+      );
+    }
+
     return Scaffold(
       body: SafeArea(
-        child: ListView(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(5.0),
-              child: Text(
-                "Current location",
-                style: TextStyle(
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.w500,
+        child: SmartRefresher(
+          enablePullUp: false,
+          enablePullDown: true,
+          controller: _refreshController,
+          onRefresh: () {
+            setState(() {});
+            _refreshController.refreshCompleted();
+          },
+          child: ListView(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(5.0),
+                child: Text(
+                  "Current location",
+                  style: TextStyle(
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
               ),
-            ),
-            //tutaj masz w _futurelookup dac swoje
-            _futureLookup(repo.getBroadcastForGPS()),
-            Padding(
-              padding: EdgeInsets.all(5.0),
-              child: Text(
-                "Saved locations",
-                style: TextStyle(
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.w500,
+              _futureLookup(repo.getBroadcastForGPS()),
+              Padding(
+                padding: EdgeInsets.all(5.0),
+                child: Text(
+                  "Saved locations",
+                  style: TextStyle(
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
               ),
-            ),
-            FutureBuilder(
-              future: _forecasts,
-              builder: (context, data) {
-                if(data.hasData){
-                  var _lookups = List<Widget>();
-
-                  for(var forecast in data.data){
-                    _lookups.add(_futureLookup(forecast, favorite: true));
+              FutureBuilder(
+                future: _forecasts,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    if (snapshot.hasData) {
+                      var _lookups = List<Widget>();
+                      for (var forecast in snapshot.data) {
+                        _lookups.add(_futureLookup(
+                          forecast,
+                          favorite: true,
+                          dismissible: true,
+                        ));
+                      }
+                      return Column(
+                        children: _lookups,
+                      );
+                    } else if (snapshot.hasError) {
+                      toast("Problem with connection to database");
+                      return Column();
+                    }
                   }
-
-                  return Column(children: _lookups,);
-                }
-                else if(data.hasError){
-                  //error handling with connection to database
-                  return Text("Connection with database error");
-                }
-                return CircularProgressIndicator();
-              },
-            ),
-          ],
+                  return CircularProgressIndicator();
+                },
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.search),
         onPressed: () {
           _searchDialog(context, repo);
-//          _getCurrentLocation();
         },
       ),
     );
   }
 }
 
-Widget _futureLookup(Future<ForecastEntity> forecast, {bool favorite = false}) {
-  return FutureBuilder(
-    future: forecast,
-    builder: (context, data) {
-      if (data.hasData) {
-        (data.data as ForecastEntity).favorite = favorite;
-        return ForecastLookup(forecast: data.data);
-      } else if (data.hasError) {
-        //error handling with connecting to api or internet connection
-        return Column();
-      }
-      return Center(
-        child: CircularProgressIndicator(),
-      );
-    },
-  );
-}
 void _searchDialog(BuildContext context, Repository repo) {
   bool _loading = false;
   bool _errorText = false;
@@ -118,68 +147,68 @@ void _searchDialog(BuildContext context, Repository repo) {
     context: context,
     barrierDismissible: true,
     builder: (BuildContext context) {
-      return StatefulBuilder(
-          builder: (context, setState){
-            return AlertDialog(
-              title: Text('Search city'),
-              content: TextField(
-                controller: cityToSearch,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'City',
-                  errorText: _errorText ? 'No matching city': null,
-                  suffixIcon: IconButton(
-                    onPressed: () => cityToSearch.clear(),
-                    icon: Icon(Icons.clear),
-                  ),
+      return StatefulBuilder(builder: (context, setState) {
+        return AlertDialog(
+          title: Text('Search city'),
+          content: TextField(
+              controller: cityToSearch,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'City',
+                errorText: _errorText ? 'No matching city' : null,
+                suffixIcon: IconButton(
+                  onPressed: () => cityToSearch.clear(),
+                  icon: Icon(Icons.clear),
                 ),
-                  textInputAction: TextInputAction.search,
-                textCapitalization: TextCapitalization.words,
-                onSubmitted: (cityName){
-                  setState((){
-                    if(!_loading)
-                      _loading = true;
-                  });
-                  Future<ForecastEntity> forecastFoundFuture = repo.getBroadcastForCity(cityToSearch.text);
-                  forecastFoundFuture.then((data) {
-                    _loading = false;
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => ForecastPage(forecast: data)));
-                  });
-                }
               ),
-              actions: [
-                FlatButton(
-                  child: Text('Cancel'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                ),
-                FlatButton(
-                  color: Colors.blue,
-                  child: btnWithLoading(_loading),
-                  onPressed: () {
-                    setState((){
-                      if(!_loading)
-                        _loading = true;
-                    });
-                    Future<ForecastEntity> forecastFoundFuture = repo.getBroadcastForCity(cityToSearch.text);
-                    forecastFoundFuture.then((data) {
-                      _loading = false;
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => ForecastPage(forecast: data)));
-                    });
-                  },
-                ),
-              ],
-            );
-          }
-      );
+              textInputAction: TextInputAction.search,
+              textCapitalization: TextCapitalization.words,
+              onSubmitted: (cityName) {
+                setState(() {
+                  if (!_loading) _loading = true;
+                });
+                Future<ForecastEntity> forecastFoundFuture =
+                    repo.getBroadcastForCity(cityToSearch.text);
+                forecastFoundFuture.then((data) {
+                  _loading = false;
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ForecastPage(forecast: data)));
+                });
+              }),
+          actions: [
+            FlatButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            FlatButton(
+              color: Colors.blue,
+              child: btnWithLoading(_loading),
+              onPressed: () {
+                setState(() {
+                  if (!_loading) _loading = true;
+                });
+                Future<ForecastEntity> forecastFoundFuture =
+                    repo.getBroadcastForCity(cityToSearch.text);
+                forecastFoundFuture.then((data) {
+                  _loading = false;
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ForecastPage(forecast: data)));
+                });
+              },
+            ),
+          ],
+        );
+      });
     },
   );
 }
+
 Widget btnWithLoading(_loading) {
   if (!_loading) {
     return new Text(
@@ -189,7 +218,7 @@ Widget btnWithLoading(_loading) {
         fontSize: 16.0,
       ),
     );
-  } else{
+  } else {
     return CircularProgressIndicator(
       valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
     );
